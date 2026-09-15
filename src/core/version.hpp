9f1482cd6be7a9d652efcb42c9022f3556284404
tom <261801702+techrote@users.noreply.github.com>
2026-09-16 00:33:36 +0100

    AM-001: add product version constants

---
+++ src/core/version.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <string_view>
+
+namespace artminer::core {
+
+inline constexpr std::string_view kProductName = "ArtMiner";
+inline constexpr std::string_view kVersion = "0.1.0-dev";
+inline constexpr std::string_view kVersionDetail = "AM-001 bootstrap";
+
+}  // namespace artminer::core
