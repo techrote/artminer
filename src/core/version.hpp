a4e674541ab3a047380042ddd78c329eae1bf592
tom <261801702+techrote@users.noreply.github.com>
2026-09-16 11:43:48 +0100

    AM-015 set release version metadata

--- src/core/version.hpp
+++ src/core/version.hpp
@@ -5,7 +5,8 @@
 namespace artminer::core {
 
 inline constexpr std::string_view kProductName = "ArtMiner";
-inline constexpr std::string_view kVersion = "0.1.0-dev";
-inline constexpr std::string_view kVersionDetail = "AM-009 deterministic export and provenance";
+inline constexpr std::string_view kVersion = "1.0.0";
+inline constexpr std::string_view kVersionDetail = "AM-015 release-hardened portable Windows build";
+inline constexpr std::string_view kReleaseChannel = "stable";
 
 }  // namespace artminer::core
